ba20445f370bbbf5d18708eddfe839a840ca973f
Aaron Csetter <aaron@csetter.com>
2023-04-02 11:03:49 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# CSC 532 Final Research Project
+> Comparing Sequential and Parallel Algorithm Performance for Solving the Maximum Clique Problem
+
+### Authors
+* Aaron Csetter
+* 
+* 
+* 
---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+"""
+** CSC 532 Final Research Project **
+Comparing Sequential and Parallel Algorithm Performance for Solving the Maximum Clique Problem
+By: Aaron Csetter, Dmytro Dobryin, Ian Pena, and Nathan Davis
+UNCW: Spring 2023
+"""
+import sys
+
+
+def main() -> None:
+    """
+    Main entry point of program.
+    """
+    sys_args = sys.argv
+    num_args = len(sys_args)
+    pass
+
+
+if __name__ == '__main__':
+    main()
